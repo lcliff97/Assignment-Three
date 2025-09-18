33a298124f76262b04eacf9c91cb34e885b1bd29
lcliff97 <louiecliff97@outlook.com>
2025-09-18 14:34:59 +0100

    counters in javascript

---
+++ app.js
@@ -0,0 +1,9 @@
+let cookies = 1;
+
+let cookiesPerClick = 1;
+
+let cps = 1;
+
+const cookieElement = document.getElementById("cookie");
+const cookiesPerClickElement = document.getElementById("cookies per click");
+const cookiesPerSecondElement = document.getElementById("cps");
