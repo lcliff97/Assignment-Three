c6fe5e8e79e7b7001e8e3d12c333d72c34932be2
lcliff97 <louiecliff97@outlook.com>
2025-09-19 11:41:55 +0100

    cookie counter implemented

--- app.js
+++ app.js
@@ -4,22 +4,20 @@ let cookiesPerClick = 1;
 
 let cps = 1;
 
+const cookieButton = document.getElementById("cookiebutton");
+const cookiesElement = document.getElementById("cookies");
 const cookieElement = document.getElementById("cookie");
 const cookiesPerClickElement = document.getElementById("cookies-per-click");
 const cookiesPerSecondElement = document.getElementById("cps");
 
-cookieElement.addEventListener(
-  "click",
-  () => {
-    cookiebutton + cookiesPerClick;
-    cookieElement.textContent = `cookiebutton: ${cookies}`;
-  },
-  1000
-);
+cookieElement.addEventListener("click", () => {
+  cookies + cookiesPerClick;
+  cookieElement.textContent = `cookies: ${cookies}`;
+});
 
 setInterval(function () {
   cookies += cps;
-  console.log(cookies);
+  cookiesElement.textContent = `Cookies: ${cookies}`;
 }, 1000);
 
 fetch("https://cookie-upgrade-api.vercel.app/api/upgrades").then((response) => {
